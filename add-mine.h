6a82626d2262be36b189b96b9c9da79fa3766aaa
Erfan-ram <Erfanramezany245@gmail.com>
2022-12-20 14:04:55 +0330

    some changes to add-mine

--- add-mine.h
+++ add-mine.h
@@ -3,6 +3,8 @@ class first
 private:
     /* data */
 public:
-    first(/* args */);
-
+    int addition(int *num1, int *num2)
+    {
+        return *num1 + *num2;
+    }
 };
--- calculator.cpp
+++ calculator.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+// #include "add-mine.h"
+// #include "devi-to.h"
+#include </mnt/nvme1n1p1/cpp-projects/calculator/add-mine.h>
+
+first *obj = new first;
 
 using namespace std;
 
 int main()
 {
-    cout << "in the name of Allah";
+    int num1, num2;
+    char opt = ' ';
+
+    cout << "please enter the calculation expression and press enter";
+    cin >> num1 >> opt >> num2;
+
+    if (opt == '+')
+    {
+        cout << obj->addition(&num1, &num2);
+    }
 
     return 0;
 }
