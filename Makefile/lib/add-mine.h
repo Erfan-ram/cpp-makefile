f5baca458d856942f9b2c0b68236f2b8ae89916a
Erfan-ram <Erfanramezany245@gmail.com>
2022-12-20 16:45:06 +0330

    essential upgrade

--- Makefile/lib/add-mine.h
+++ Makefile/lib/add-mine.h
@@ -3,11 +3,11 @@ class first
 public:
     auto addition(auto *num1, auto *num2)
     {
-        return *num1 * *num2;
+        return *num1 + *num2;
     }
 
     auto subtraction(auto *num1, auto *num2)
     {
-        return *num1 / *num2;
+        return *num1 - *num2;
     }
 };
