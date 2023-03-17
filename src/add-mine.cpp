5e55c1851339f9a340475c91b698bc2fd0f4b653
Erfan Ramezani <Erfanramezany245@gmail.com>
2023-03-17 21:10:12 +0330

    add-mine clean code implented

--- src/add-mine.cpp
+++ src/add-mine.cpp
@@ -1,13 +1,11 @@
-class first
-{
-public:
-    auto addition(auto *num1, auto *num2)
-    {
-        return *num1 + *num2;
-    }
+#include "add-mine.h"
 
-    auto subtraction(auto *num1, auto *num2)
-    {
-        return *num1 - *num2;
-    }
-};
+auto addition(auto *num1, auto *num2)
+{
+    return *num1 + *num2;
+}
+
+auto subtraction(auto *num1, auto *num2)
+{
+    return *num1 - *num2;
+}
